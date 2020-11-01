3a275b003f17d75781f9f9102b82785d0ef4538c
Jonathan Gamble <jdgamble555@gmail.com>
2020-10-31 23:38:00 -0500

    auto create aggregate doc

--- src/shared/joins.ts
+++ src/shared/joins.ts
@@ -187,6 +187,12 @@ export async function aggregateData(
     data[aggregateField].push(d);
   });
   console.log('Aggregating ', colId, ' data on ', targetRef.path);
+  // create doc if it doesn't exist
+  const aggregateDoc = await targetRef.get();
+  if (!aggregateDoc.exists) {
+    aggregateDoc.ref.create({});
+  }
+  // add aggregate information
   await targetRef.set(data, { merge: true }).catch((e: any) => {
     console.log(e);
   });
